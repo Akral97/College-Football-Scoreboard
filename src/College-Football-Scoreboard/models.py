3479b00352eb15dc49e8a239fad93ffc189ca2e2
Adam Kral <adamkral@adams-mbp.lan>
2022-03-28 21:37:37 -0700

    started work on Game class

---
+++ src/College-Football-Scoreboard/models.py
@@ -0,0 +1,11 @@
+class Game:
+
+    def __init__(self, date, neutralSite, homeTeam, finished, team_1, team_2, team_1_score, team_2_score):
+        self.date = date
+        self.neutralSite =neutralSite
+        self.homeTeam = homeTeam
+        self.finished = finished
+        self.team_1 = team_1
+        self.team_2 = team_2
+        self.team_1_score = team_1_score
+        self.team_2_score = team_2_score
